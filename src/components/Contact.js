e6248ff591957e638002b42f6ecd701ddf5d7fba
nicholas570 <nicolas.mercier570@gmail.com>
2020-04-05 15:22:00 +0200

    convert function component to class component and add online state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,49 @@
 import React from "react";
 import "./Contact.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-function Contact(props) {
-  return (
-    <div className="Contact">
-      <img
-        className="avatar"
-        src={props.avatar}
-        alt="contact's avatar"
-      />
-      <div>
-        <h4 className="name">{props.name}</h4>
-        <div className="status">
-          <div className={props.online  ? 'status-online' : 'status-offline'}></div>
-          <p className="status-text">{props.online ? 'Online' : 'Offline'}</p>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: this.props.online,
+    };
+  }
+
+  render() {
+    return (
+      <div className="Contact">
+        <img
+          className="avatar"
+          src={this.props.avatar}
+          alt="contact's avatar"
+        />
+        <div>
+          <h4 className="name">{this.props.name}</h4>
+          <div
+            className="status"
+            onClick={(event) => {
+              const newOnline = !this.state.online;
+              this.setState({ online: newOnline });
+            }}>
+            <div
+              className={
+                this.state.online ? "status-online" : "status-offline"
+              }></div>
+            <p className="status-text">
+              {this.state.online ? "Online" : "Offline"}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Contact.propTypes = {
   name: PropTypes.string,
   avatar: PropTypes.string,
-  online: PropTypes.bool
+  online: PropTypes.bool,
 };
 
 export default Contact;
